f7bfb81ecd56d69b377b7c95d9a5a8df4d9a94bb
dortusrkaus1 <qlcsk0147@naver.com>
2024-11-10 18:48:31 +0900

    [fix] 공간 추가 텍스트 오류 수정

--- src/pages/areaAdd/AreaAdd.js
+++ src/pages/areaAdd/AreaAdd.js
@@ -144,43 +144,43 @@ const AreaAdd = () => {
 
     return (
         <Layout>
-            <div className="input__container-wrapper">
-                <div className="input__container" style={{ display: 'flex', alignItems: 'center', gap: '15px' }}>
-                    <div className="shadow__input"></div>
+            <div className="area_input__container-wrapper">
+                <div className="area_input__container" style={{ display: 'flex', alignItems: 'center', gap: '15px' }}>
+                    <div className="area_shadow__input"></div>
                     <input
                         type="text"
                         name="areaname"
-                        className="input__search"
+                        className="area_input__search"
                         placeholder="ex) 우리집"
                         value={areaName}
                         onChange={handleAreaNameChange}
                         required
                         style={{ flex: 1 }}
                     />
-                    <button className="input__button__shadow" onClick={handleSubmit} type="button" disabled={user_no === null}>
+                    <button className="area_input__button__shadow" onClick={handleSubmit} type="button" disabled={user_no === null}>
                         저장
                     </button>
                 </div>
-                {error && <p className="error-text">{error}</p>}
+                {error && <p className="area_error-text">{error}</p>}
             </div>
-            <div className="spaces-list">
+            <div className="area_spaces-list">
                 <h2>공간 목록</h2>
                 {loading ? (
                     <p>로딩 중...</p>
                 ) : Array.isArray(spaces) && spaces.length > 0 ? (
                     <ul>
                         {spaces.map((space, index) => (
-                            <li key={space.area_no ? space.area_no : index} className="space-item">
-                                <span className="space-name">{space.area_name}</span>
-                                <div className="buttons-container">
-                                    <button className="editBtn" onClick={() => handleEditSpace(space.area_no)}>
+                            <li key={space.area_no ? space.area_no : index} className="area_space-item">
+                                <span className="area_space-name">{space.area_name}</span>
+                                <div className="area_buttons-container">
+                                    <button className="area_editBtn" onClick={() => handleEditSpace(space.area_no)}>
                                         <svg height="1em" viewBox="0 0 512 512">
                                             <path
                                               d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
                                             ></path>
                                         </svg>
                                     </button>
-                                    <button className="button deleteBtn" onClick={() => handleDeleteSpace(space.area_no)}>
+                                    <button className="area_button deleteBtn" onClick={() => handleDeleteSpace(space.area_no)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 69 14" className="svgIcon bin-top">
                                             <g clipPath="url(#clip0_35_24)">
                                                 <path
